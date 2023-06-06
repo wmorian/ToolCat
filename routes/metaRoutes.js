9f253a0c03f740fb889561ca4aa6867231757eed
Wahid <wmorian@pm.me>
2023-06-06 16:38:24 +0200

    catch error when no metaTags available.

--- routes/metaRoutes.js
+++ routes/metaRoutes.js
@@ -5,25 +5,29 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 const getMetaData = async (url) => {
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const metaTags = {
-        title: $('head title').text(),
-        description: $('meta[name=description]').attr('content'),
-        ogTitle: $('meta[property="og:title"]').attr('content'),
-        ogDescription: $('meta[property="og:description"]').attr('content'),
-        ogImage: $('meta[property="og:image"]').attr('content'),
-    };
+    try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+        const metaTags = {
+            title: $('head title').text(),
+            description: $('meta[name=description]').attr('content'),
+            ogTitle: $('meta[property="og:title"]').attr('content'),
+            ogDescription: $('meta[property="og:description"]').attr('content'),
+            ogImage: $('meta[property="og:image"]').attr('content'),
+        };
 
-    return metaTags;
+        return metaTags;
+    } catch (error) {
+        return [];
+    }
 };
 
 router.post('/', async (req, res) => {
     const { url } = req.body;
 
     getMetaData(url)
-        .then(tags => {
-            res.json(tags)
+        .then(metaTags => {
+            res.json(metaTags)
         })
         .catch(console.error);
 });
